bc3c98ddb68909ab6ae7a1af06d7636d67f688fe
Saurabh Kumar Yadav <132044380+saurabhskills@users.noreply.github.com>
2024-02-19 16:28:14 +0530

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,62 +1,63 @@
 #include<iostream>
 using namespace std;
+
 class student {
-    private: 
+private: 
     char name[30];
     int roll, regNo;
     
-    public:
+public:
     void getdata();//method declaration
     void putdata();
 };
 
-class person{
-    private:
+class person {
+private:
     char name[30];
     char gender[10];
     
-    public:
-    void putdata(){
-        cout<<"Enter your name: ";
+public:
+    void setdata(){
+        cout<<"Enter your name: "<<endl;
         cin.getline(name,30);
         cout<<"Enter your gender: ";
-        cin>>gender;
+        cin >> gender;
+        cin.ignore(); // to consume the newline character left in the input buffer
     }
     void getdata(){
         cout<<"The name of the person is: "<<name<<endl;
         cout<<"The gender of the person is: "<<gender<<endl;  
     }
-    
 };
 
 //We have two ways to define the methods of a class.
 //Either we can declare and define the methods in the class itself.(example see in the class person )
 //Or we can define the method outside the class.
 
-
-void student :: getdata(){
+void student::getdata(){
     cout<<"The name of the student is: "<<name<<endl;
     cout<<"The roll of the student is: "<<roll<<endl;
     cout<<"The registration number of the student is: "<<regNo<<endl;
 }
-void student :: putdata(){
+
+void student::putdata(){
     cout<<"Enter the name:";
     cin.getline(name, 30);
     cout<<"Enter the roll number:";
-    cin>>roll;
+    cin >> roll;
     cout<<"Enter your registration number:";
-    cin>>regNo;
+    cin >> regNo;
+    cin.ignore(); // to consume the newline character left in the input buffer
 }
-
 
 int main(){
     
     student s1;
     s1.putdata();
-    s1.getdata();
+    s1.getdata();   
     
     person p1;
-    p1.putdata();
+    p1.setdata();  
     p1.getdata();
     
     return 0;
